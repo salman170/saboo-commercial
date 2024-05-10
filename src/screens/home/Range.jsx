f439f24a9c401a2d3d47c2990189c2a178b3942a
salman170 <salmanansari02120@gmail.com>
2024-05-10 14:55:05 +0530

    done with range

--- src/screens/home/Range.jsx
+++ src/screens/home/Range.jsx
@@ -10,12 +10,11 @@ import "swiper/css/navigation";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { Autoplay, Navigation } from "swiper/modules";
 import CountUp from "react-countup";
-import View360 from "../../components/View360";
 
 const Range = () => {
   const [selected, setSelected] = useState(1);
   const [activeSlide, setActiveSlide] = useState(0);
-  const [threesixty, setThreesixty] = useState(0);
+
   return (
     <div
       style={{
@@ -96,68 +95,55 @@ const Range = () => {
         </div>
       </div>
       <div className="z-10 py-16 ">
-        {threesixty ? (
-          <div className="flex justify-center ">
-            <div className="w-80  md:w-[40rem] -mb-10  ">
-              <View360
-                path={
-                  "https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/saboonexa/Ciaz/360"
-                }
-                count={36}
-                format={"jpg"}
+        <Swiper
+          //   loop={true}
+          centeredSlides={true}
+          // navigation={true}
+          // autoplay={{
+          //   delay: 2000,
+          //   disableOnInteraction: false,
+          // }}
+          modules={[Autoplay, Navigation]}
+          breakpoints={{
+            // when window width is >= 640px
+            // 640: {
+            //   slidesPerView: 1,
+            //   spaceBetween: 20,
+            // },
+            // when window width is >= 768px
+            768: {
+              slidesPerView: 1.9,
+              spaceBetween: 10,
+            },
+            // when window width is >= 1024px
+            1024: {
+              slidesPerView: 1.9,
+              spaceBetween: 10,
+            },
+          }}
+          className="w-full carSlider"
+          onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
+        >
+          <SwiperSlide>
+            <div className="flex items-center justify-center ">
+              <img
+                src={require("../../assets/vehicles/super_carry/Super Carry.webp")}
+                alt=""
+                srcset=""
               />
             </div>
-          </div>
-        ) : (
-          <Swiper
-            //   loop={true}
-            centeredSlides={true}
-            // navigation={true}
-            // autoplay={{
-            //   delay: 2000,
-            //   disableOnInteraction: false,
-            // }}
-            modules={[Autoplay, Navigation]}
-            breakpoints={{
-              // when window width is >= 640px
-              // 640: {
-              //   slidesPerView: 1,
-              //   spaceBetween: 20,
-              // },
-              // when window width is >= 768px
-              768: {
-                slidesPerView: 1.9,
-                spaceBetween: 10,
-              },
-              // when window width is >= 1024px
-              1024: {
-                slidesPerView: 1.9,
-                spaceBetween: 10,
-              },
-            }}
-            className="w-full carSlider"
-            onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
-          >
-            <SwiperSlide>
-              <div className="flex items-center justify-center ">
-                <img
-                  src={require("../../assets/vehicles/super_carry/Super Carry.webp")}
-                  alt=""
-                  srcset=""
-                />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="flex items-center justify-center ">
-                <img
-                  src={require("../../assets/vehicles/ecco_cargo/Eeco Cargo.webp")}
-                  alt=""
-                  srcset=""
-                />
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        )}
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex items-center justify-center ">
+              <img
+                src={require("../../assets/vehicles/ecco_cargo/Eeco Cargo.webp")}
+                alt=""
+                srcset=""
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+
         <div className="flex items-center justify-center w-full h-full -mt-24 -z-10">
           <img
             src={require("../../assets/home/dotted-circle.png")}
@@ -166,16 +152,8 @@ const Range = () => {
             className="md:h-28 w-min"
           />
         </div>
-        <div className="flex items-center justify-center w-full h-full -mt-12 opacity-0 md:opacity-100 -z-10 md:z-20">
-          <img
-            onClick={() => setThreesixty(!threesixty)}
-            src={require("../../assets/home/Icon360.svg").default}
-            alt=""
-            srcset=""
-            className="z-20 h-24 cursor-pointer"
-          />
-        </div>
-        <div className="flex items-center justify-center h-6 mt-10 md:mt-4">
+
+        <div className="flex items-center justify-center h-6 mt-10 ">
           {activeSlide ? (
             <img
               src={require("../../assets/vehicles/ecco_cargo/eecoCargoBrandName.png")}
@@ -191,15 +169,6 @@ const Range = () => {
               className="h-6 w-min"
             />
           )}
-        </div>
-        <div className="flex items-center justify-center w-full h-full md:hidden">
-          <img
-            onClick={() => setThreesixty(1)}
-            src={require("../../assets/home/Icon360.svg").default}
-            alt=""
-            srcset=""
-            className="h-24 cursor-pointer "
-          />
         </div>
       </div>
     </div>
