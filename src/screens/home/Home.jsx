aec2e124d4e205dc7f9f0659863682e26e7685bd
salman170 <salmanansari02120@gmail.com>
2024-07-01 14:59:57 +0530

    done with homepage

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,96 +1,96 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import HeroSection from "./HeroSection";
 import Range from "./Range";
-// import { useGSAP } from "@gsap/react";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import AboutUs from "./AboutUs";
-// import HomeNews from "./HomeNews";
-// import SocialMedia from "./SocialMedia";
+import AboutUs from "./AboutUs";
+import HomeNews from "./HomeNews";
+import SocialMedia from "./SocialMedia";
 import Enquiry from "./Enquiry";
-// import ScrollingSection from "./ScrollingSection";
-// import ModalYoutube from "../../components/ModalYoutube";
+import ScrollingSection from "./ScrollingSection";
+import ModalYoutube from "../../components/ModalYoutube";
 
 // Ensure ScrollTrigger is registered with GSAP
 gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
-  // const [openYoutube, setOpenYoutube] = useState({
-  //   open: false,
-  //   title: "",
-  //   url: "",
-  //   description: "",
-  // });
+  const [openYoutube, setOpenYoutube] = useState({
+    open: false,
+    title: "",
+    url: "",
+    description: "",
+  });
 
-  // useGSAP(() => {
-  //   // New animation for.slider_img that triggers on scroll
-  //   gsap.fromTo(
-  //     ".slider_img",
-  //     { opacity: 0, y: 100, duration: 2, ease: "power1.inOut" },
-  //     {
-  //       opacity: 1,
-  //       duration: 2,
-  //       y: 0,
-  //       ease: "power1.inOut",
+  useGSAP(() => {
+    // New animation for.slider_img that triggers on scroll
+    gsap.fromTo(
+      ".slider_img",
+      { opacity: 0, y: 100, duration: 2, ease: "power1.inOut" },
+      {
+        opacity: 1,
+        duration: 2,
+        y: 0,
+        ease: "power1.inOut",
 
-  //       scrollTrigger: {
-  //         trigger: ".slider_img",
-  //         start: "top 95%",
-  //         end: "bottom bottom",
-  //         // start: "bottom 10%", // Adjust this value to control when the animation starts
-  //         // end: "bottom center", // Adjust this value to control when the animation ends
-  //         scrub: true, // Smoothly transition the animation based on scroll position
-  //         // markers: true, // Show markers for testing purposes
-  //       },
-  //     }
-  //   );
+        scrollTrigger: {
+          trigger: ".slider_img",
+          start: "top 95%",
+          end: "bottom bottom",
+          // start: "bottom 10%", // Adjust this value to control when the animation starts
+          // end: "bottom center", // Adjust this value to control when the animation ends
+          scrub: true, // Smoothly transition the animation based on scroll position
+          // markers: true, // Show markers for testing purposes
+        },
+      }
+    );
 
-  //   // gsap.to(".scorllingCar", {
-  //   //   duration: 4,
-  //   //   right: "10%",
-  //   //   ease: "power1.inOut",
-  //   //   scrollTrigger: {
-  //   //     trigger: ".scorllingCar",
-  //   //     start: "top 50%",
-  //   //     end: "bottom bottom",
-  //   //     // start: "bottom 10%", // Adjust this value to control when the animation starts
-  //   //     // end: "bottom center", // Adjust this value to control when the animation ends
-  //   //     scrub: true, // Smoothly transition the animation based on scroll position
-  //   //     // markers: true, // Show markers for testing purposes
-  //   //   },
-  //   // });
+    // gsap.to(".scorllingCar", {
+    //   duration: 4,
+    //   right: "10%",
+    //   ease: "power1.inOut",
+    //   scrollTrigger: {
+    //     trigger: ".scorllingCar",
+    //     start: "top 50%",
+    //     end: "bottom bottom",
+    //     // start: "bottom 10%", // Adjust this value to control when the animation starts
+    //     // end: "bottom center", // Adjust this value to control when the animation ends
+    //     scrub: true, // Smoothly transition the animation based on scroll position
+    //     // markers: true, // Show markers for testing purposes
+    //   },
+    // });
 
-  //   // timeline.to(".scorllingDiv", {
-  //   //   duration: 2,
-  //   //   height: "200vh", // Adjust this value to control the height of the "scrollingDiv
-  //   //   y: 0,
-  //   //   ease: "power1.inOut",
-  //   //   scrollTrigger: {
-  //   //     trigger: ".scorllingDiv",
-  //   //     start: "top 85%",
-  //   //     end: "bottom bottom",
-  //   //     // start: "bottom 10%", // Adjust this value to control when the animation starts
-  //   //     // end: "bottom center", // Adjust this value to control when the animation ends
-  //   //     scrub: true, // Smoothly transition the animation based on scroll position
-  //   //     markers: true, // Show markers for testing purposes
-  //   //   },
-  //   // });
-  // });
+    // timeline.to(".scorllingDiv", {
+    //   duration: 2,
+    //   height: "200vh", // Adjust this value to control the height of the "scrollingDiv
+    //   y: 0,
+    //   ease: "power1.inOut",
+    //   scrollTrigger: {
+    //     trigger: ".scorllingDiv",
+    //     start: "top 85%",
+    //     end: "bottom bottom",
+    //     // start: "bottom 10%", // Adjust this value to control when the animation starts
+    //     // end: "bottom center", // Adjust this value to control when the animation ends
+    //     scrub: true, // Smoothly transition the animation based on scroll position
+    //     markers: true, // Show markers for testing purposes
+    //   },
+    // });
+  });
 
   return (
     <div className="overflow-hidden">
       <HeroSection />
       <Enquiry title1="ENQUIRE NOW " />
        <Range />
-     {/* <AboutUs setOpen={setOpenYoutube} />
+      <AboutUs setOpen={setOpenYoutube} />
       <HomeNews />
       <SocialMedia />
-      <ScrollingSection />
+     <ScrollingSection />
       <ModalYoutube
         open={openYoutube.open}
         data={openYoutube}
         setOpenYoutube={setOpenYoutube}
-      /> */}
+      /> 
     </div>
   );
 };
